c4a6a852cc422e33640c14302b9f4464752c8a78
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-19 16:22:56 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.take.allow_create_artists #the instance of the preference for allow_create_artists is true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
